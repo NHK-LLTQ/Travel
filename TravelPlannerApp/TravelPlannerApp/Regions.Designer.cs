1eaf999864eb5eb602b8162760d966bf6e4185cf
PhanHoangf <17520519@gm.uit.edu.vn>
2018-12-21 20:17:48 +0700

    Update

---
+++ TravelPlannerApp/TravelPlannerApp/Regions.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TravelPlannerApp
+{
+    partial class Regions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Regions));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnbac = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btntrung = new System.Windows.Forms.Button();
+            this.btnnam = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(387, 608);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnbac
+            // 
+            this.btnbac.BackColor = System.Drawing.Color.Lime;
+            this.btnbac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbac.Location = new System.Drawing.Point(76, 629);
+            this.btnbac.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnbac.Name = "btnbac";
+            this.btnbac.Size = new System.Drawing.Size(198, 52);
+            this.btnbac.TabIndex = 2;
+            this.btnbac.Text = "Miền Bắc";
+            this.btnbac.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(398, 14);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(411, 608);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(817, 14);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(381, 608);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btntrung
+            // 
+            this.btntrung.BackColor = System.Drawing.Color.Lime;
+            this.btntrung.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntrung.Location = new System.Drawing.Point(492, 629);
+            this.btntrung.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btntrung.Name = "btntrung";
+            this.btntrung.Size = new System.Drawing.Size(204, 52);
+            this.btntrung.TabIndex = 5;
+            this.btntrung.Text = "Miền Trung";
+            this.btntrung.UseVisualStyleBackColor = false;
+            // 
+            // btnnam
+            // 
+            this.btnnam.BackColor = System.Drawing.Color.Lime;
+            this.btnnam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnam.Location = new System.Drawing.Point(938, 629);
+            this.btnnam.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnnam.Name = "btnnam";
+            this.btnnam.Size = new System.Drawing.Size(188, 52);
+            this.btnnam.TabIndex = 6;
+            this.btnnam.Text = "Miền Nam";
+            this.btnnam.UseVisualStyleBackColor = false;
+            // 
+            // Regions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnnam);
+            this.Controls.Add(this.btntrung);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnbac);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Regions";
+            this.Text = "Regions";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnbac;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btntrung;
+        private System.Windows.Forms.Button btnnam;
+    }
+}
